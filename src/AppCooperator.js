b9aff65bbb6e4f3eb51c5291e1864d138bf2bb68
agix <florian.gaultier@gmail.com>
2019-05-24 01:26:59 +0200

    Build cooperator

---
+++ src/AppCooperator.js
@@ -0,0 +1,67 @@
+import React from 'react'
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'font-awesome/css/font-awesome.min.css'
+
+import 'react-big-calendar/lib/css/react-big-calendar.css'
+import 'react-datepicker/dist/react-datepicker.css'
+import { Row } from 'react-bootstrap';
+import './styles.css'
+import './prism.css'
+import CooperatorCalendar from './cooperator/CooperatorCalendar'
+import CooperatorHeader from './cooperator/CooperatorHeader'
+import {authFetch, logout} from './common/utils'
+
+class App extends React.Component {
+
+  constructor(props, context) {
+    super(props, context);
+    this.changeRoute = this.changeRoute.bind(this)
+    this.state = {
+      whoami: null,
+      route: document.location.hash,
+    }
+  }
+
+  changeRoute() {
+    this.setState({route: document.location.hash})
+  }
+
+  componentDidMount() {
+    window.addEventListener("hashchange", this.changeRoute, false);
+    authFetch(`${process.env.REACT_APP_BACKEND}/v0/whoami`)
+      .then(whoami => {
+        this.setState({whoami})
+      })
+  }
+
+  render() {
+    if(this.state.whoami !== null) {
+      let element = <CooperatorCalendar whoami={this.state.whoami}/>
+      let header = <CooperatorHeader whoami={this.state.whoami}/>
+      return (
+        <div className="app">
+          <div className="jumbotron">
+            <img src="/logo.png" />
+            <div className="container" style={({display: 'inline-grid', textAlign: 'center'})}>
+              <Row>
+                Bonjour {this.state.whoami.pseudo}, bienvenue chez Permis de Construire. <a href="/" onClick={logout}>Se déconnecter</a> <a href={`${process.env.REACT_APP_BACKEND}/v0/activities.pdf`} target="_blank"> Télécharger l'agenda </a>
+              </Row>
+            </div>
+          </div>
+          <div className="examples">
+            <div className="example">
+              {element}
+            </div>
+          </div>
+        </div>
+      )
+    }
+
+    return (
+      <div></div>
+    )
+  }
+}
+
+export default App;
